9bd50a78272dd67fe2d1d22a99c2f140b6f40458
octobervale <valesmith@u.boisestate.edu>
2024-11-19 14:49:57 -0700

    created index

--- Index.js
+++ Index.js
@@ -1,1 +1,16 @@
-Index.js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Define a route for the home page
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
